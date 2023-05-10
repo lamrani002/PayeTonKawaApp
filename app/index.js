3aa251a8bd272dee587b04b48a2820cc846c2eb0
Lamrani Abdel <lamrani002@gmail.com>
2023-05-10 04:55:55 +0200

    login without scaning each time

--- app/index.js
+++ app/index.js
@@ -1,5 +1,33 @@
-import { Redirect } from "expo-router";
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 
-export default function Index() {
-    return <Redirect href="/started" />;
+export default function Index() { 
+        const [isLoading, setIsLoading] = useState(true);
+         const [isValidToken, setIsValidToken] = useState(false); 
+         useEffect(() => { 
+            const checkToken = async () => {
+            const token = await AsyncStorage.getItem('token');
+            if (token) { 
+            try { 
+                const response = await axios.get('https://apiepsierp.herokuapp.com/validate-token', { headers: { Authorization: `Bearer ${token}`, }, });
+                    if (response.status === 200) {
+                        setIsValidToken(true); 
+                    } 
+                } catch (error) {
+                        console.error('Error validating token:', error); 
+                    } 
+                } 
+                setIsLoading(false);
+                }; 
+                checkToken(); 
+            }, []); 
+            if (isLoading) { 
+                return null;
+            }
+            if (isValidToken) {
+                return <Redirect href="/home" />;
+                }
+            return <Redirect href="/started" />;
 }
